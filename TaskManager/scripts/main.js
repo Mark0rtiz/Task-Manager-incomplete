1f9b87f44a14392827266859c5fde136c1a37e90
Mark0rtiz <106716676+Mark0rtiz@users.noreply.github.com>
2022-07-31 15:46:01 -0700

    Add files via upload

--- TaskManager/scripts/main.js
+++ TaskManager/scripts/main.js
@@ -39,20 +39,73 @@ function saveTask() {
     status,
     notification
   );
-
-  displayTask(newTask);
+  //send task to the server
+  $.ajax({
+    type: "POST",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/",
+    data: JSON.stringify(newTask),
+    contentType: "application/json",
+    success: function (res) {
+      console.log("server says", res);
+      displayTask(newTask);
+    },
+    error: function (errorDetails) {
+      console.log("error saving tasks", errorDetails);
+    },
+  });
 }
 
-function displayTask(newTask) {
-  let syntax = `<div class = "task"> 
-  <h4>${newTask.title} ${newTask.importance}</h4>
-  <p>${newTask.description}</p>
-  <div>${newTask.dueDate}</div>
-  <div>${newTask.location}</div>
-  <div><h5>${newTask.status}</h5></div>
-  </div>`;
+function displayTask(task) {
+  let syntax = `<div class="task" style= "border: 2px solid #353535;">  
+      <i class="fa-regular fa-star"></i>
+  
+      <div class="info">
+        <h5>${task.title}</h5>
+        <p>${task.description}</p>
+      </div>
+  
+      <label class="location">${task.location}</label>
+  
+      <label class="date">${task.dueDate}</label>
+  
+      <label class="status">${task.status}</label>
+  
+    </div>`;
 
   $("#pendingTasks").append(syntax);
+}
+
+function testRequest() {
+  $.ajax({
+    type: "GET",
+    url: "https://fsdiapi.azurewebsites.net/",
+    success: function (res) {
+      console.log("server says", res);
+    },
+    error: function (errorDetails) {
+      console.log("error", errorDetails);
+    },
+  });
+}
+
+function fetchTasks() {
+  $.ajax({
+    type: "GET",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks",
+    success: function (res) {
+      console.log("server says", res);
+      let tasks = JSON.parse(res); //array of task objects
+      for (let i = 0; i < tasks.length; i++) {
+        let task = tasks[i];
+        if (task.name == "Mark") {
+          displayTask(task);
+        }
+      }
+    },
+    error: function (details) {
+      console.log("error retrieving tasks", details);
+    },
+  });
 }
 
 function init() {
@@ -62,6 +115,7 @@ function init() {
   $("#btnSave").click(saveTask);
 
   //load intitial data
+  fetchTasks();
 }
 
 window.onload = init;
